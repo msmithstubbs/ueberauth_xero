f975941fea65e904a0c396d67fe663485bfe277d
Matt Stubbs <matt@yellowrobot.com.au>
2023-11-15 10:56:30 +1000

    Prepare for publishing paceage to hex

--- mix.exs
+++ mix.exs
@@ -1,13 +1,16 @@
 defmodule UeberauthXero.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/msmithstubbs/ueberauth_xero"
   @version "0.1.0"
 
   def project do
     [
       app: :ueberauth_xero,
       version: @version,
+      name: "Üeberauth Xero",
       elixir: "~> 1.13",
+      package: package(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -27,6 +30,18 @@ defmodule UeberauthXero.MixProject do
       {:jose, "~> 1.11.0"},
       {:credo, ">= 0.0.0", only: [:dev, :test], runtime: false},
       {:ex_doc, ">= 0.0.0", only: [:dev], runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      description: "An Uberauth strategy for Xero OAuth2 authentication.",
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Matt Stubbs"],
+      licenses: ["MIT"],
+      links: %{
+        GitHub: @source_url
+      }
     ]
   end
 end
